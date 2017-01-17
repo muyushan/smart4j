41455cecf6410d636afb2432fa591bb16d580395
muyushan <muyushanm@126.com>
2017-01-17 18:44:24 +0800

    发发发

--- chapter2/src/main/java/org/sane/chapter2/HelloServlet.java
+++ chapter2/src/main/java/org/sane/chapter2/HelloServlet.java
@@ -7,12 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 /**
  * Servlet implementation class HelloServlet
  */
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+  Logger logger=  Logger.getLogger(Log4JInitServlet.class);
+	private static final long serialVersionUID = 1L; 
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -27,6 +30,7 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		logger.info("fffffffffffffffffffff");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
